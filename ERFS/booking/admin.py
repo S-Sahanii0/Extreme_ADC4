c9b11dd9f90af9bb568a8a9bad3ecfc48cca7e0e
yuzu0o0 <55910981+yuzu0o0@users.noreply.github.com>
2020-01-11 08:26:05 +0545

    Creating table

--- ERFS/booking/admin.py
+++ ERFS/booking/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Asset, Booking, AddToFav
 
-# Register your models here.
+admin.site.register(Asset)
+admin.site.register(Booking)
+admin.site.register(AddToFav)
--- ERFS/booking/models.py
+++ ERFS/booking/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from endUser.models import Seller, Buyer
 
-# Create your models here.
+class Asset(models.Model):
+    seller=models.ForeignKey(Seller,on_delete=models.CASCADE)
+    asset_Title=models.CharField(max_length=50)
+    asset_Type=models.CharField(max_length=20)
+    asset_Price=models.IntegerField()
+    asset_purpose=models.CharField(max_length=10)
+    asset_Location=models.CharField(max_length=50)
+    asset_Image=models.ImageField()
+    def __str__(self):
+        return self.asset_Title
+class Booking(models.Model):
+    asset=models.OneToOneField(Asset,on_delete=models.CASCADE)
+    buyer=models.ForeignKey(Buyer,on_delete=models.CASCADE)
+    booking_Date=models.DateTimeField()
+    booking_Status= models.BooleanField()
+    def __str__(self):
+        return self.booking_Date
+class AddToFav(models.Model):
+    buyer=models.ManyToManyField(Buyer)
+    asset=models.ManyToManyField(Asset)
+    def __str__(self):
+        return self.buyer
