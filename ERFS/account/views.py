6cbb7bedb6bebbe9b3825f8e8b323b43d5ba862f
Drt36 <58885633+Drt36@users.noreply.github.com>
2020-01-11 15:02:39 +0545

    working in views

--- ERFS/account/views.py
+++ ERFS/account/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from endUser.models import Buyer,Seller
 
 # Create your views here.
+
+def index(request):
+    return render(request=request,template_name="main/index.html",)
+
+def register(request):
+    return HttpResponse("hello register")
+
+def login(request):
+    return HttpResponse("hello login")
+
+def logout(request):
+    return HttpResponse("hello logout")
