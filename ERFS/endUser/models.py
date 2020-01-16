281c300a53bd277ff633d297d305822f501be71b
Drt36 <58885633+Drt36@users.noreply.github.com>
2020-01-16 19:02:44 +0545

    update

--- ERFS/endUser/models.py
+++ ERFS/endUser/models.py
@@ -2,17 +2,25 @@ from django.db import models
 # Create your models here.
 
 class Seller(models.Model):
+    userName=models.CharField(max_length=10)
     seller_Name=models.CharField(max_length=30)
     seller_Contact=models.IntegerField()
     seller_Address=models.CharField(max_length=40)
+    password=models.CharField(max_length=20)
+    confirm_password=models.CharField(max_length=20)
+
+    
 
     def __str__(self):
-        return self.seller_Name
+        return self.userName
 
 class Buyer(models.Model):
+    userName=models.CharField(max_length=10)
     buyer_Name=models.CharField(max_length=30)
     buyer_Contact=models.IntegerField()
     buyer_Address=models.CharField(max_length=40)
+    password=models.CharField(max_length=20)
+    confirm_password=models.CharField(max_length=20)
 
     def __str__(self):
-        return self.buyer_Name
+        return self.userName
