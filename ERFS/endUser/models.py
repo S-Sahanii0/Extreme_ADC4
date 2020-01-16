f9e97c006137edf56ef2f45b6bd0c14f68f488d0
Drt36 <58885633+Drt36@users.noreply.github.com>
2020-01-16 19:54:54 +0545

    update

--- ERFS/endUser/models.py
+++ ERFS/endUser/models.py
@@ -2,7 +2,7 @@ from django.db import models
 # Create your models here.
 
 class Seller(models.Model):
-    userName=models.CharField(max_length=10)
+    username=models.CharField(max_length=10)
     seller_Name=models.CharField(max_length=30)
     seller_Contact=models.IntegerField()
     seller_Address=models.CharField(max_length=40)
@@ -12,10 +12,10 @@ class Seller(models.Model):
     
 
     def __str__(self):
-        return self.userName
+        return self.username
 
 class Buyer(models.Model):
-    userName=models.CharField(max_length=10)
+    username=models.CharField(max_length=10)
     buyer_Name=models.CharField(max_length=30)
     buyer_Contact=models.IntegerField()
     buyer_Address=models.CharField(max_length=40)
@@ -23,4 +23,4 @@ class Buyer(models.Model):
     confirm_password=models.CharField(max_length=20)
 
     def __str__(self):
-        return self.userName
+        return self.username
