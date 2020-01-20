0eca8bbae0aac0c9282656fa4ef3752960009ad6
Drt36 <58885633+Drt36@users.noreply.github.com>
2020-01-20 08:46:17 +0545

    update

--- ERFS/account/views.py
+++ ERFS/account/views.py
@@ -44,17 +44,18 @@ def registerS(request):
 
 def loginB(request):
     if request.method=='POST':
-        f2=AuthenticationForm(request.POST)
-        username2=f2.cleaned_data.get['username']
-        password2=f2.cleaned_data.get['password']
-        buyer= authenticate(username=username2,password=password2)
-        if buyer is not None:
-            login(request,buyer)
-            return redirect('account:book')
+        f2=AuthenticationForm(request=request,data=request.POST)
+        if f2.is_valid():
+            username2=f2.cleaned_data.get('username')
+            password2=f2.cleaned_data.get('password')
+            buyer= authenticate(request,username=username2,password=password2)
+            if buyer is not None:
+                    login(request,buyer)
+                    return redirect('account:book')
                 
         else:
             return render(request,"main/loginB.html",{'error':"Invalid Username and Password."})
-        
+            
     else:
         f2=AuthenticationForm()
         return render(request,"main/loginB.html",{"f2":f2})
