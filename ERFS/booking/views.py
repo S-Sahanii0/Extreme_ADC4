5c581019302cef49883fa1e87dfdbd942a720365
yuzu0o0 <55910981+yuzu0o0@users.noreply.github.com>
2020-01-26 20:49:11 +0545

    db

--- ERFS/booking/views.py
+++ ERFS/booking/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect, get_object_or_404
 from .models import Asset
 from .forms import UploadForm
+from django.http import HttpResponse
 
 
 def upload(request):
@@ -31,9 +32,16 @@ def delete_asset(request, pk= None):
     asset.delete()
     return redirect('booking:display')
 
-#def fav_list(request,id):
-    #asset = get_object_or_404(Asset, id=id)
-    #if request.method == 'POST':
-        #asset.favourite.add(request.user)
-    #return render(request, "uploads/details.html", {"assets": asset})
-# Create your views here.
+def fav_list(request,id):
+    is_favorite = False
+    if asset.favorite.filter(id=request.user.id).exists():
+        is_favorite = True
+    context = {
+        "is_favorite":is_favorite
+    }
+    asset = get_object_or_404(Asset, id=id)
+    if asset.favorite.filter(id= request.user.id).exists():
+        asset.favorite.remove(request.user)
+    else:
+        asset.favourite.add(request.user)
+    return redirect(asset.get_absolute_url())
