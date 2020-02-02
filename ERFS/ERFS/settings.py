387d7e3c9535864f75fce240ff76c3029af4afe6
yuzu0o0 <55910981+yuzu0o0@users.noreply.github.com>
2020-02-02 08:41:33 +0545

    update

--- ERFS/ERFS/settings.py
+++ ERFS/ERFS/settings.py
@@ -124,5 +124,4 @@ MEDIA_URL = '/assets/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT= os.path.join(BASE_DIR, 'images')
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 MEDIA_URL="/profile/images/"
