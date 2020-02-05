2226ece81cc69ae9792f3d0578acd3d77925eef3
ajaltdr <58896248+ajaltdr@users.noreply.github.com>
2020-02-05 19:17:33 +0545

    Search implementation

--- ERFS/Asset/urls.py
+++ ERFS/Asset/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 app_name = "Asset"
-urlpatterns = [
-    path('home/',views.home,name="home"),
-    path('search/',views.search,name="search"),
+urlpatterns = [ 
+    path('result/',views.result,name="result")
     
 ]
--- ERFS/Asset/views.py
+++ ERFS/Asset/views.py
@@ -1,28 +1,34 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.shortcuts import render,redirect
 from .models import Asset
 from django.contrib import messages
 from django.db.models import Q
+from django import template
+
 
 
 # Create your views here.
-def home(request):
-    return render(request, "home.html",)
 
-def search(request):
-    if request.method=='POST':
-        srch = request.POST['srh']
 
-        if srch:
-            match = Asset.objects.filter(Q(asset_Title__startswith=srch) | Q(asset_Title__icontains=srch))
 
-            if match:
-                return render(request,'search.html',{'sr':match})
-            else:
-                messages.error(request,'no result found')
-        else: 
-            return HttpResponseRedirect('/search/')         
-    return render(request,'search.html')  
+def result(request):
+    query =" "
+    context = {}
+    if request.GET:
+        query = request.GET['q']
+    asset = get_data_queryset(query)
+    context['asset'] = asset
+    
+    return render(request, "result.html", context)  
+
+def get_data_queryset(query=None):
+    queryset = []
+    queries = query.split(" ")
+    for q in queries:
+        assets = Asset.objects.filter(Q(asset_Title__startswith=q) | Q(asset_Title__icontains=q))
+
+        for asset in assets:
+            queryset.append(asset)
+    return list(set(queryset))            
+
+
+
